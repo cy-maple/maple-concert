fe63e78057b3beea3a35854bac2770ec5945fa3a
cy-maple <1424657318@qq.com>
2024-01-12 16:11:45 +0800

    feat: test

--- concert-web/src/pages/home/index.tsx
+++ concert-web/src/pages/home/index.tsx
@@ -131,7 +131,7 @@ function Home() {
   };
   useEffect(() => {
     // 连接socket
-    socket = io("http://localhost:3000", {
+    socket = io("http://123.56.215.24:3000", {
       query: {
         room: room,
         user: user,
